80228ea6b348d90c49eb3684e9df2c37be5e4e9c
Drdiz <drdiz@mail.ru>
2022-07-12 11:29:53 +0300

    функция рассчета факиориала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n < 2 ) {
+    return 1;
+   } else {
+    let i = n;
+    while (i > 1) {
+      --i;
+      n = n * i;
+    }
+    return n;
+  }
 }
