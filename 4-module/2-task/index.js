b3a7b890a60a14b02244642cbe914c3fb09d8280
Drdiz <drdiz@mail.ru>
2022-07-27 13:36:39 +0300

    4-й модуль,  задача 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+    let tableRows = table.querySelectorAll('tr');
+    row = 0;
+    tableRows.forEach(tableRow => {
+        tableRow.children[row].style.background = 'red';
+        row++;        
+        });      
 }
