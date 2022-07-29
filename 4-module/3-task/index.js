7da5f55d9b4b466a18331f23619e5ca49fd3e2ce
Drdiz <drdiz@mail.ru>
2022-07-29 11:31:33 +0300

    4-й модуль,  задача 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+    let tableRows = table.rows;
+    let StatusData, GenderData, AgeData, row = 0;
+
+    for (row = 1; row < tableRows.length; row++) {
+
+        StatusData = table.rows[row].cells[3].getAttribute("data-available");
+        GenderData = table.rows[row].cells[2].textContent;
+        AgeData = parseInt(table.rows[row].cells[1].textContent);
+
+        //Gender
+        (GenderData == "m") ? table.rows[row].classList.add('male') : table.rows[row].classList.add('female');
+        // Status
+        if (StatusData == null) {
+            table.rows[row].setAttribute('hidden', '')
+        } else if (StatusData == "true") {
+            table.rows[row].classList.add('available')
+        } else {
+            table.rows[row].classList.add('unavailable')
+        }
+        //Age
+        if (AgeData < 18) {
+            table.rows[row].setAttribute('style', 'text-decoration: line-through')
+        }
+    }
 }
