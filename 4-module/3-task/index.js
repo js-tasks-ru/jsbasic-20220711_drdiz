87d034bee4e01f71a96d272c6baf6bc7c69008f5
Drdiz <drdiz@mail.ru>
2022-08-01 08:26:28 +0300

    4-й модуль,  задача 4-3, корректировка 1

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,26 +1,22 @@
 function highlight(table) {
-    let tableRows = table.rows;
-    let StatusData, GenderData, AgeData, row = 0;
+    let statusData, genderData, ageData, row = 0;
 
-    for (row = 1; row < tableRows.length; row++) {
-
-        StatusData = table.rows[row].cells[3].getAttribute("data-available");
-        GenderData = table.rows[row].cells[2].textContent;
-        AgeData = parseInt(table.rows[row].cells[1].textContent);
-
-        //Gender
-        (GenderData == "m") ? table.rows[row].classList.add('male') : table.rows[row].classList.add('female');
-        // Status
-        if (StatusData == null) {
+    for (row = 1; row < table.rows.length; row++) {
+        statusData = table.rows[row].cells[3].dataset.available;
+        genderData = table.rows[row].cells[2].textContent;
+        ageData = parseInt(table.rows[row].cells[1].textContent);
+        (genderData == "m") ? table.rows[row].classList.add('male') : table.rows[row].classList.add('female');
+        
+        if (statusData == null) {
             table.rows[row].setAttribute('hidden', '')
-        } else if (StatusData == "true") {
+        } else if (statusData == "true") {
             table.rows[row].classList.add('available')
         } else {
             table.rows[row].classList.add('unavailable')
         }
-        //Age
-        if (AgeData < 18) {
-            table.rows[row].setAttribute('style', 'text-decoration: line-through')
+        
+        if (ageData < 18) {
+            table.rows[row].style.textDecoration = "line-through";
         }
     }
 }
