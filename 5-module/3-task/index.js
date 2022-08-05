ac084d4ae57ed0cf95b8cc93cdd7c599a1138fa4
Drdiz <drdiz@mail.ru>
2022-08-05 14:37:05 +0300

    5-й модуль,  задача 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,20 @@
 function initCarousel() {
-  // ваш код...
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let slide = document.querySelector('.carousel__inner');
+  let step = 0;
+  let slideWdth = slide.offsetWidth;
+  let carouselRightArrow = document.querySelector('.carousel__arrow_right');
+  let carouselLeftArrow = document.querySelector('.carousel__arrow_left');
+
+  carouselLeftArrow.style.display = 'none';
+  leftArrow.addEventListener('click', () => Sliderunner(1));
+  rightArrow.addEventListener('click', () => Sliderunner(-1));
+
+  function Sliderunner (direction) {    
+    step += slideWdth * direction;
+    slide.style.transform = `translateX(${step}px)`;
+    (step == -3 * slideWdth) ? carouselRightArrow.style.display = 'none' : carouselRightArrow.style.display = '';
+    (step == 0) ? carouselLeftArrow.style.display = 'none' : carouselLeftArrow.style.display = '';
+  }
 }
